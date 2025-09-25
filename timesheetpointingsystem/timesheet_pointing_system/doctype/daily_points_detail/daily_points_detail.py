af8a4597543cad7903f45a83ca0679a3b5a81c1d
Kanishkar <kanishvijay2005@gmail.com>
2025-09-25 17:51:29 +0530

    Remove some doctype

--- timesheetpointingsystem/timesheet_pointing_system/doctype/daily_points_detail/daily_points_detail.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Kanishkar and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class DailyPointsDetail(Document):
-	pass
